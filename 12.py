3bc2f070c183c6727ea5b83800ba8b5c0742c5fe
Muskan112 <54355333+Muskan112@users.noreply.github.com>
2019-08-21 15:25:45 +0530

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,47 @@
+'''a=int(input("Enter a number = "))
+b=int(input("Enter a number = "))
+
+c=a+b
+'''
+'''
+print(c)
+fib=1000
+a,b=0,1
+while a<fib:
+    print(a)
+    a,b=b,a+b
+'''
+'''
+a=int(input("Enter a number = "))
+b=int(input("Enter a number = "))
+c=a
+a=b
+b=c
+print(a)
+print(b)
+'''
+'''s='PACIFIC'
+for a in s:
+    print(a,end=' ')
+'''
+a=int(input('Enter a number = '))
+
+while True:
+    word = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
